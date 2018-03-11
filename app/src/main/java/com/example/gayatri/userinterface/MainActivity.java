fcf5276db2472e0e1accae0cc8f0fd96ba8acabf
Unknown <athul1998balakrishnan@gmail.com>
2018-03-11 11:51:17 +0530

    Backend

--- app/src/main/java/com/example/gayatri/userinterface/MainActivity.java
+++ app/src/main/java/com/example/gayatri/userinterface/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.gayatri.userinterface;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.util.SparseArray;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,13 +19,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     ImageButton cam;
     ImageButton gallery;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int GALLERY_REQUEST = 2;
+    String mCurrentPhotoPath;
+    ImageView mImageView;
+    Bitmap bitmap;
+    Uri photoURI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,28 +51,29 @@ public class MainActivity extends AppCompatActivity
 
         cam=findViewById(R.id.cam);
         gallery=findViewById(R.id.gallery);
+        mImageView =(ImageView) findViewById(R.id.imageView);
 
         cam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Works",Toast.LENGTH_LONG).show();
+                dispatchTakePictureIntent();
             }
         });
 
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"Works",Toast.LENGTH_LONG).show();
+               gallery();
             }
         });
-        FloatingActionButton fab =findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //FloatingActionButton fab =findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer =findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -105,5 +131,124 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.about_id){
         }
         return true;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.gayatri.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(null);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mImageView.setImageBitmap(imageBitmap);
+            setPic();
+
+        }
+        else if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            Uri selectedImageUri = data.getData();
+            //mImageView.setImageURI(selectedImageUri);
+            mCurrentPhotoPath = selectedImageUri.getPath();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+            //setPic();
+        }
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+    }
+
+    public void gallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent.createChooser(intent,"Select File"), GALLERY_REQUEST);
+//        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+//        photoPickerIntent.setType("image/*");
+//        startActivityForResult(photoPickerIntent,  REQUEST_TAKE_PHOTO);
+    }
+
+    public void ocr(View view) {
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+//        if (!textRecognizer.isOperational()) {
+//            Toast.makeText(getApplicationContext(), "Couldn't load the Text", Toast.LENGTH_SHORT).show();
+//        } else {
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<TextBlock> item = textRecognizer.detect(frame);
+
+        StringBuilder text = new StringBuilder(1000);
+
+        //for (int i = 0; i < item.size(); i++) {
+//                if(item.valueAt(0).equals('\n')) {
+//                    text.replace(i,i+1," ");
+//                }
+        TextBlock myitems = item.valueAt(0);
+        text.append(myitems.getValue());
+        //}
+        Intent intent = new Intent(MainActivity.this, NewActivity.class);
+        intent.putExtra("data", text.toString());
+        startActivity(intent);
     }
 }
